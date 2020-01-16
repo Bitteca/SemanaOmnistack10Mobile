1fbf1ff024ba7ef594a8ce5c23b74f803283090e
Bitteca <biteca@gmail.com>
2020-01-16 20:03:16 -0300

    App Finalizado.

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -9,6 +9,8 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+import api from '../services/api';
+
 import MapView, { Marker, Callout } from 'react-native-maps';
 import {
   requestPermissionsAsync,
@@ -19,8 +21,10 @@ import { MaterialIcons } from '@expo/vector-icons';
 
 const Main = ({ navigation }) => {
   const [currentRegion, setCurrentRegion] = useState(null);
+  const [devs, setDevs] = useState([]);
+  const [techs, setTechs] = useState('');
 
-  //Carrega mapa na tela na regiao
+  //Carrega mapa na tela na regiao do usuario
   useEffect(() => {
     async function loadInitialPosition() {
       const { granted } = await requestPermissionsAsync();
@@ -35,8 +39,8 @@ const Main = ({ navigation }) => {
         setCurrentRegion({
           latitude,
           longitude,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
         });
       }
     }
@@ -44,32 +48,61 @@ const Main = ({ navigation }) => {
     loadInitialPosition();
   }, []);
 
+  async function loadDevs() {
+    const { latitude, longitude } = currentRegion;
+
+    const response = await api.get('/search', {
+      params: {
+        latitude,
+        longitude,
+        techs,
+      },
+    });
+
+    setDevs(response.data.devs);
+  }
+
+  function handleRegionChanged(region) {
+    setCurrentRegion(region);
+  }
+
   if (!currentRegion) {
     return null;
   }
 
   return (
     <>
-      <MapView initialRegion={currentRegion} style={styles.map}>
-        <Marker coordinate={{ latitude: -25.3571897, longitude: -49.2058243 }}>
-          <Image
-            style={styles.avatar}
-            source={{
-              uri:
-                'https://avatars1.githubusercontent.com/u/53625800?s=460&v=4',
-            }}
-          />
-          <Callout
-            onPress={() => {
-              navigation.navigate('Profile', { github_username: 'bitteca' });
+      <MapView
+        onRegionChangeComplete={handleRegionChanged}
+        initialRegion={currentRegion}
+        style={styles.map}>
+        {devs.map(dev => (
+          <Marker
+            key={dev._id}
+            coordinate={{
+              longitude: dev.location.coordinates[0],
+              latitude: dev.location.coordinates[1],
             }}>
-            <View style={styles.callout}>
-              <Text style={styles.devName}>Eduardo Bittencourt</Text>
-              <Text style={styles.devBio}>Desenvolvedor Web Junior.</Text>
-              <Text style={styles.devTechs}>ReactJS, React Native, NodeJS</Text>
-            </View>
-          </Callout>
-        </Marker>
+            <Image
+              style={styles.avatar}
+              source={{
+                uri: dev.avatar_url,
+              }}
+            />
+            <Callout
+              onPress={() => {
+                navigation.navigate('Profile', {
+                  github_username: dev.github_username,
+                });
+              }}>
+              <View style={styles.callout}>
+                <Text style={styles.devName}>{dev.name}</Text>
+                <Text style={styles.devBio}>{dev.bio}</Text>
+                <Text style={styles.devTechs}>{dev.techs.join(', ')}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
       </MapView>
       <View style={styles.searchForm}>
         <TextInput
@@ -78,8 +111,12 @@ const Main = ({ navigation }) => {
           placeholderTextColor='#999'
           autoCapitalize='words'
           autoCorrect={false}
+          value={techs}
+          onChangeText={text => {
+            setTechs(text);
+          }}
         />
-        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+        <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
           <MaterialIcons name='my-location' size={20} colo='#fff' />
         </TouchableOpacity>
       </View>
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://',
+  baseURL: 'http://192.168.25.19:3333',
 });
 
 export default api;
